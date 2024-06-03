5c7c9f8436307c9fa53b71e7433289daa5d8cafe
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-03 11:50:53 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,24 @@
+
+/**
+ * @param {number[][]} arrays
+ * @return {number[]}
+ */
+var longestCommonSubsequence = function (arrays) {
+    const RANGE_OF_VALUES = [1, 100];
+    const frequency = new Array(RANGE_OF_VALUES[1] + 1).fill(0);
+    const longestCommonSubsequence = new Array();
+
+    for (let numbers of arrays) {
+        for (let value of numbers) {
+            ++frequency[value];
+        }
+    }
+
+    for (let value = RANGE_OF_VALUES[0]; value <= RANGE_OF_VALUES[1]; value++) {
+        if (frequency[value] === arrays.length) {
+            longestCommonSubsequence.push(value);
+        }
+    }
+
+    return longestCommonSubsequence;
+};
